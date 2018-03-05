55ea96414c0b0b04c79c9264f2bbba58a9306653
ilovezy <1518265554@qq.com>
2018-03-05 21:55:16 +0800

    Update pages

--- components/tagList/tagList.js
+++ components/tagList/tagList.js
@@ -8,7 +8,7 @@ Component({
       type: Array,
       value: [],
     },
-    style: {
+    otherstyle: {
       type: String,
       value: ''
     }
--- pages/course/couseDetail.js
+++ pages/course/couseDetail.js
@@ -5,62 +5,138 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    selectedTab: 0,
+    swiperHeight: 200,
+    ognCourseList: [{
+      courseName: '第一节课',
+      label: 'jacksom',
+      date: '2017年7月7日 ',
+      courseIntroduce: '非常非常好的课程',
+      prizeMin: 21,
+      totalParticipate: 300
+    }, {
+      courseName: 'fuck',
+      label: 'jacksom',
+      courseIntroduce: '非常非常好的课程',
+      prizeMin: 21,
+      date: '2017年7月7日 ',
+
+      totalParticipate: 300
+    }, {
+      courseName: 'fuck',
+      label: 'jacksom',
+      courseIntroduce: '非常非常好的课程',
+      prizeMin: 21,
+      date: '2017年7月7日 ',
+      totalParticipate: 300,
+      disabled: true
+    }, {
+      courseName: 'fuck',
+      label: 'jacksom',
+      date: '2017年7月7日 ',
+      courseIntroduce: '非常非常好的课程',
+      prizeMin: 21,
+      totalParticipate: 300
+    }],
+
+    studentList: [{
+      avatar: 'http://res.xiaomaiketang.com/xiaomai/fatherDay_20170607.png',
+      name: 'Cc',
+      count: 6
+    }, {
+      avatar: '',
+      name: '你好好哈',
+      count: 6
+    }, {
+      avatar: '',
+      name: 'jack',
+      count: 61
+    }, {
+      avatar: 'http://res.xiaomaiketang.com/xiaomai/fatherDay_20170607.png',
+      name: 'fuck',
+      count: 336
+    }]
+  },
+
+  selectTab(e) {
+    const self = this
+    var tab = e.currentTarget.dataset.tab
+    self.setData({
+      selectedTab: tab
+    })
+  },
+
+  changeSwiper(e) {
+    let current = e.detail.current
+    this.setData({
+      selectedTab: current
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var self = this;
+    //  高度自适应
+    wx.getSystemInfo({
+      success: function (res) {
+        var clientHeight = res.windowHeight,
+          clientWidth = res.windowWidth;
+        var calc = clientHeight - 50; // TODO 这里有点操蛋
+        self.setData({
+          swiperHeight: calc
+        });
+      }
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
