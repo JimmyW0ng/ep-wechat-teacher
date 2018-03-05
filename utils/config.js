a30872f283413d1c3f6f21bdf325b38f84d5a753
misCC <1518265554@qq.com>
2018-03-05 18:27:44 +0800

    update

--- utils/config.js
+++ utils/config.js
@@ -3,14 +3,13 @@ let token = User.getToken()
 let memberType = User.getMemberType()
 
 const CONFIG = {
-  // apiDocUrl: 'http://122.225.218.26:9009/swagger-ui.html',
   apiDocUrl: 'http://ep2.viphk1.ngrok.org/swagger-ui.html',
-  // apiUrl: 'http://122.225.218.26:9009/',
   apiUrl: 'http://ep2.viphk1.ngrok.org/',
   
   mySqlUrl: 'jdbc:mysql://122.225.218.26:9002/ep?useUnicode=true&characterEncoding=utf-8&useSSL=false',
 
   testPhone: 13575785566,
+  teacherPhone: 18000000001,
   clientId: 'wechat_app',
   clientSecret: '123456',
 
