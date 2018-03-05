297b287650babb7679cbc0fbd4af5b7adb93ba23
misCC <1518265554@qq.com>
2018-03-05 17:42:07 +0800

    update

--- pages/child/childDetail.js
+++ pages/child/childDetail.js
@@ -5,62 +5,68 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    childNickName: '哈哈哈哈',
+    sign: '嘿嘿嘿拉风已达UFO阿嘿嘿嘿拉风已达UFO阿父爱嘿嘿嘿拉风已达UFO阿父爱嘿嘿嘿拉风已达UFO阿父爱父爱',
+    totalElements: 0,
+    tags: [{
+      tagName: 'fuck',
+      num: 2
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
